281cab24ffc1676912372176e727568667aab870
Rishabh jangra <Rishabhjangra2002@gmail.com>
2025-09-20 23:45:27 +0530

    push

--- frontend/features/productsApi.js
+++ frontend/features/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const productsApi = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_BASE }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/' }),
   tagTypes: ['Products'],
   endpoints: (builder) => ({
     getProducts: builder.query({
